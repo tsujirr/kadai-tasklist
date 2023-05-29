6c3163647ad17ce2cf4b3fdb70441e0739d65cfe
sumire.mizuma <sumire.mizuma@grp.tmnf.jp>
2023-05-29 15:21:17 +0900

    getAllMessagesを追加

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,27 +7,34 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "tasks")
-public class Tasks{
+public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "created_at",nullable = false)
+    @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
-    @Column(name = "updated_at",nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
-    @Column(name = "content",length = 255,nullable = false)
+    @Column(name = "content", length = 255, nullable = false)
     private String content;
 
 
-    //getter/setter
     public Integer getId() {
         return id;
     }
